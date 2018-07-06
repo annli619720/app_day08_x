f0a74a3693cc515c6579694425570eb579f071f6
annli619720 <annli_619720@163.com>
2018-07-06 19:57:27 +0800

    更改描述

--- Scripts/test_001.py
+++ Scripts/test_001.py
@@ -7,5 +7,15 @@ class Test_001:
     @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
     @allure.step(title='第一个测试')
     def test_001_1(self):
-        allure.attach('这是一个描述', '描述的内容')
+        allure.attach('描述', '尝试登录')
         assert 0
+
+    @pytest.allure.severity(pytest.allure.severity_level.NORMAL)
+    @allure.step(title='第二一个测试')
+    def test_001_2(self):
+        allure.attach('描述', '输入用户名')
+        allure.attach('描述', '输入密码')
+        allure.attach('描述', '点击登录')
+        assert 1
+
+
