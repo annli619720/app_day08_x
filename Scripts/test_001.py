8824f39a2e6e99453a484a87ac4555305143b6ca
annli619720 <annli_619720@163.com>
2018-07-06 19:22:45 +0800

    添加严重程度和描述内容

--- Scripts/test_001.py
+++ Scripts/test_001.py
@@ -4,6 +4,8 @@ import allure
 
 class Test_001:
 
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
     @allure.step(title='第一个测试')
     def test_001_1(self):
+        allure.attach('这是一个描述', '描述的内容')
         assert 0
